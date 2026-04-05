f686c5c3085bb1fd06b8f2da9425750fcadebbe0
Thunderlol <the.real.thunderlol@gmail.com>
2026-04-05 23:38:54 +0500

    update recursive_extractor.py

--- recursive_extractor.py
+++ recursive_extractor.py
@@ -1,20 +1,21 @@
 import struct
 import os
-from custom_libraries import linkdata_extract,popup,g1t_extractor
+from custom_libraries import linkdata_extract,popup,g1t_extractor, extract_g1m
+
 
 import time
 
 start_time = time.time()  # start timer
 ### select file
-FILE = popup.select_file([
-     ("BIN FILES", "*.BIN")
-])
+# FILE = popup.select_file([
+#      ("BIN FILES", "*.BIN")
+# ])
 
-#FILE = "LINKDATA_A.BIN"
+FILE = "LINKDATA_A.BIN"
 
 ### after selection, select output folder
-OUTPUT = popup.select_folder()
-#OUTPUT = "output/"
+# OUTPUT = popup.select_folder()
+OUTPUT = "output/"
 os.makedirs(OUTPUT, exist_ok=True)
 
 ### extract toc
@@ -25,7 +26,10 @@ linkdata_extract.decompress_files(OUTPUT,TOC)
 linkdata_extract.assign_extensions(OUTPUT, TOC)
 
 # git_extractor can go throught the entire directory or a single file
+
 g1t_extractor.folder_extract(OUTPUT)
+
+extract_g1m.extract_g1m_containers_in_folder(OUTPUT)
 
 
 end_time = time.time()  # end timer
